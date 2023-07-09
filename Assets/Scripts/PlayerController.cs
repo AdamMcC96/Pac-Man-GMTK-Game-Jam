4e91e1ef176ae2c92f36b2a066e88358763cec57
AdamMcC96 <adammccarthy96@hotmail.com>
2023-07-09 12:53:17 +0100

    Added a PlayerController script to handle movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public Transform orientation;
+    float xInput;
+    float yInput;
+
+    public float groundDrag;
+    Vector3 moveDir;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        MovePlayer();
+    }
+    private void Update()
+    {
+        PlayerInput();
+        SpeedControl();
+        rb.drag = groundDrag;
+    }
+    private void PlayerInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDir = orientation.forward * yInput + orientation.right * xInput;
+        rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
